f8c725d6ccc5e5c9d21e60b74e379838b1634ca1
Mujibullah-Rahimi <89380319+Mujibullah-Rahimi@users.noreply.github.com>
2022-03-04 15:26:16 +0100

    feat : TileManager implements GameObject interface

--- src/no/hiof/game2d/tile/TileManager.java
+++ src/no/hiof/game2d/tile/TileManager.java
@@ -1,7 +1,15 @@
 package no.hiof.game2d.tile;
 
+import no.hiof.game2d.Canvas;
+import no.hiof.game2d.GameObject;
+
 import java.io.File;
 
-public class TileManager {
+public class TileManager implements GameObject {
     public void loadMap(File mapFile){}
+
+    @Override
+    public void draw(Canvas canvas, int positionX, int positionY) {
+
+    }
 }
