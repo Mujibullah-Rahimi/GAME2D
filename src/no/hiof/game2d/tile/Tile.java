8bc4eb1a259899d164185330ffa5d3425abe62f9
Mujibullah-Rahimi <89380319+Mujibullah-Rahimi@users.noreply.github.com>
2022-03-04 08:44:09 +0100

    feat : Tile class

---
+++ src/no/hiof/game2d/tile/Tile.java
@@ -0,0 +1,11 @@
+package no.hiof.game2d.tile;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+
+    public boolean collision = false;
+    public BufferedImage image;
+
+    public Tile(BufferedImage image, boolean collision){}
+}
