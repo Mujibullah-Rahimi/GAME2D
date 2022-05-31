9560815683963be9bb89ea0df9df450cacb6d906
Mujibullah-Rahimi <89380319+Mujibullah-Rahimi@users.noreply.github.com>
2022-05-31 21:51:05 +0200

    fix: InternalGamePanel implements Runnable, and has methods to run gameThread

--- src/game2d/internal/InternalGamePanel.java
+++ src/game2d/internal/InternalGamePanel.java
@@ -3,17 +3,40 @@ package game2d.internal;
 import javax.swing.*;
 import java.awt.*;
 
-public class InternalGamePanel extends JPanel {
+public class InternalGamePanel extends JPanel implements Runnable {
+    Thread gameThread;
 
+    // Constructors
     public InternalGamePanel(){
         this.setPreferredSize(new Dimension(500,500));
+        this.setDoubleBuffered(true);
     }
-
     public InternalGamePanel(int width, int height){
         this.setPreferredSize(new Dimension(width,height));
+        this.setDoubleBuffered(true);
     }
 
-    public void paint(Graphics g){
+    public void startMyGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void run(){
+        while (gameThread != null){
+            // UPDATE: update information such as placement of elements
+            update();
+            // DRAW: draw the screen with the updated information
+            repaint();
+            System.out.println("The game loop is running");
+        }
+    }
+
+    public void update(){
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
     }
 }
