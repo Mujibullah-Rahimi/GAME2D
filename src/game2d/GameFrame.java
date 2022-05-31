b0a5c73c45316d24726e749112fffe891d537c90
Mujibullah-Rahimi <89380319+Mujibullah-Rahimi@users.noreply.github.com>
2022-05-31 21:52:43 +0200

    fix: fixed constructor to take title for frame

--- src/game2d/GameFrame.java
+++ src/game2d/GameFrame.java
@@ -2,18 +2,14 @@ package game2d;
 
 import game2d.internal.InternalGameFrame;
 
-import javax.swing.*;
-
-public class GameFrame extends JFrame {
+public class GameFrame{
     private InternalGameFrame frame;
 
-
+    // Constructors
     public GameFrame(){
         this.frame = new InternalGameFrame();
     }
-
-    public GameFrame(int width, int height){
-        this.frame = new InternalGameFrame(width, height);
+    public GameFrame(int width, int height, String title){
+        this.frame = new InternalGameFrame(width, height, title);
     }
-
 }
