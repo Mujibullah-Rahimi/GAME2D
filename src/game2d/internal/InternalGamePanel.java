3a99f3ba7dba9b8117d60407b46d44c16a8c31c5
Mujibullah-Rahimi <89380319+Mujibullah-Rahimi@users.noreply.github.com>
2022-06-03 11:40:33 +0200

    fix: added some default values, and Keylistener to gamepanel-constructors

--- src/game2d/internal/InternalGamePanel.java
+++ src/game2d/internal/InternalGamePanel.java
@@ -1,19 +1,31 @@
 package game2d.internal;
 
+import game2d.Keyhandler;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class InternalGamePanel extends JPanel implements Runnable {
     Thread gameThread;
+    Keyhandler keyHandler = new Keyhandler();
+    // Set default position for player
+    int playerX = 100;
+    int playerY = 100;
+    int movementSpeed = 5;
 
     // Constructors
     public InternalGamePanel(){
         this.setPreferredSize(new Dimension(500,500));
         this.setDoubleBuffered(true);
+
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
     public InternalGamePanel(int width, int height){
         this.setPreferredSize(new Dimension(width,height));
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void startMyGameThread(){
@@ -32,11 +44,26 @@ public class InternalGamePanel extends JPanel implements Runnable {
     }
 
     public void update(){
-
+        if (keyHandler.wPressed){
+            playerY -= movementSpeed;
+        }
+        else if (keyHandler.sPressed){
+            playerY += movementSpeed;
+        }
+        else if (keyHandler.dPressed){
+            playerX -= movementSpeed;
+        }
+        else if (keyHandler.aPressed){
+            playerX += movementSpeed;
+        }
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.black);
+        g2d.fillRect(playerX, playerY, 48, 48);
+        g2d.dispose();
     }
 }
