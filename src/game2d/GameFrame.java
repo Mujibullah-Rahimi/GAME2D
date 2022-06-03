4f3ed9bed86965bb087cb331a67b79c7769ca36d
Mujibullah-Rahimi <89380319+Mujibullah-Rahimi@users.noreply.github.com>
2022-06-03 11:41:21 +0200

    fix: added GamePanel parameter to constructor

--- src/game2d/GameFrame.java
+++ src/game2d/GameFrame.java
@@ -4,12 +4,11 @@ import game2d.internal.InternalGameFrame;
 
 public class GameFrame{
     private InternalGameFrame frame;
-
     // Constructors
     public GameFrame(){
         this.frame = new InternalGameFrame();
     }
-    public GameFrame(int width, int height, String title){
-        this.frame = new InternalGameFrame(width, height, title);
+    public GameFrame(int width, int height, String title, GamePanel gamePanel){
+        this.frame = new InternalGameFrame(width, height, title, gamePanel);
     }
 }
